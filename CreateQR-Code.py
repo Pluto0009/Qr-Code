73bac74a3cddd7edf10208ea2689d0f3986af914
SirZwiebel <YannickBohn555@gmail.com>
2026-02-03 18:14:00 +0000

    Position Squares Added

--- CreateQR-Code.py
+++ CreateQR-Code.py
@@ -1,25 +1,25 @@
 
+import gpu
+
 rows, cols = 25, 25
 qr_code = [[0 for _ in range(cols)] for _ in range(rows)]
 
 def prepare_code():
     create_position_square(0,0)
-    create_position_square(19,0)
-    create_position_square(0,-19)
+    create_position_square(18,0)
+    create_position_square(0,18)
 
 def create_position_square(x_start_pos: int, y_start_pos: int):
     for i in range(7):
-        qr_code[y_start_pos][x_start_pos + i] = 1
-        qr_code[y_start_pos - 6][x_start_pos + i] = 1
+        qr_code[x_start_pos + i][y_start_pos] = 1
+        qr_code[x_start_pos + i][y_start_pos + 6] = 1
     for i in range(5):
-        qr_code[y_start_pos - 1 - i][x_start_pos] = 1
-        qr_code[y_start_pos - 1 - i][x_start_pos + 6] = 1
-    o = 0
-    for i in range(3):
-        qr_code[y_start_pos - 2 - o][x_start_pos + 2 + i] = 1
-        if i == 3:
-            i = 0
-            o += 1
-        if o >= 4:
-            break
+        qr_code[x_start_pos][y_start_pos + 1 + i] = 1
+        qr_code[x_start_pos + 6][y_start_pos + 1 + i] = 1
+    for o in range(3):
+        for i in range(3):
+            qr_code[x_start_pos + 2 + i][y_start_pos + 2 + o] = 1
+
+prepare_code()
+gpu.erstelle_pixel_bild(qr_code)
         
