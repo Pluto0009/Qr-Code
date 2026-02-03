a242f9332a1cfa9eaf55abe0fdc6f9a3ff260575
Pluto0009 <stein.Leopold@yahoo.com>
2026-02-03 20:08:40 +0100

    link_to_binary erstellt, tut was es sagt

---
+++ link_to_binary.py
@@ -0,0 +1,10 @@
+def link_to_binary(url):
+    binary_string = ''  # Leer starten, ohne Leerzeichen
+    for char in url:
+        # Wir formatieren den Buchstaben und hängen ihn direkt hinten an
+        binary_string += format(ord(char), '08b') 
+    return binary_string
+
+
+
+print(link_to_binary("https://www.google.de/"))
