1bfa3ccbeb3c92b5c2c5e540afb0e287642b118c
Pluto0009 <stein.Leopold@yahoo.com>
2026-03-08 16:45:55 +0100

    Implement error correction and padding logic for QR code generation

---
+++ Reed_Solomon_error_correction.py
@@ -0,0 +1,64 @@
+import goliathfieldmathematik as gf
+import math
+
+
+
+def bitsTOint(bits: list[int]):
+    bits_reversed = bits[::-1]
+    val = sum(bit * (2**i) for i, bit in enumerate(bits_reversed))
+    return val
+
+
+def intTObits(number: int):
+    bit_list = [(number >> i) & 1 for i in range(7, -1, -1)]
+    return bit_list
+
+def Error_correction_bits_erstellen(daten: list[int]):
+
+    if len(daten) % 8 != 0:
+        print("Fuck")
+        return False
+
+
+    intList = []
+    for i in range(len(daten)/8):
+        intList.append(bitsTOint(daten[i*8:(i+1)*8]))
+
+
+    generator_polynomial = [1, 216, 194, 159, 111, 199, 94, 95, 113, 157, 193]
+
+    register = []
+
+    register = intList.copy()
+    for i in range(10):
+        register.append(0)
+
+    
+
+
+    while(len(register) > 10):
+        if register[0] == 0:
+            register.pop(0)
+        else:
+            hilfs_list = []
+            for i2 in range(len(generator_polynomial)):
+                hilfs_list.append(bitsTOint(gf.mulitplizierenGF(intTObits(register[0]), intTObits(generator_polynomial[i2]))))
+            
+            for i in range(len(hilfs_list)):
+                register[i] = bitsTOint(gf.addierenGF(intTObits(register[i]), intTObits(hilfs_list[i])))
+            
+            register.pop(0)
+    
+
+
+    finallist = []
+
+    for i in range(len(register)):
+        bitList = intTObits(register[i])
+        for i2 in range(len(bitList)):
+            finallist.append(bitList[i2])
+      
+
+    return finallist
+
+            
--- fill.py
+++ fill.py
@@ -2,40 +2,86 @@ from Fillorder import generate_qr_list
 from  link_to_binary import link_to_binary_list
 from qrcodevariable import qr_code
 from gpu import erstelle_pixel_bild
+from Reed_Solomon_error_correction import Error_correction_bits_erstellen
+
 
 
 
 
 def fillNormal(link: str):
-    qrFillorder = generate_qr_list() 
-    inputbinar = link_to_binary_list(link)          #[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,]
+    inputbinar = link_to_binary_list(link)          
 
-    if len(inputbinar) > 34*8:      # check ob es passt
-        print("Zu lang (Wie meiner)")
+    max_bits = 34 * 8 # Kapazität für Version 2, Korrekturlevel L
+    
+    # 1. Check ob es überhaupt passt
+    if len(inputbinar) > max_bits:
+        print("Zu lang!")
         return False
     
+    # --- PADDING LOGIK START ---
     
+    # 2. Terminator hinzufügen (bis zu 4 Nullen, aber nur so viele wie Platz ist)
+    terminator_bits = min(4, max_bits - len(inputbinar))
+    for _ in range(terminator_bits):
+        inputbinar.append(0)
+        
+    # 3. Auf das nächste volle Byte auffüllen (Bit-Alignment)
+    while len(inputbinar) % 8 != 0 and len(inputbinar) < max_bits:
+        inputbinar.append(0)
+        
+    # 4. Mit Padding-Bytes auffüllen (0xEC und 0x11 abwechselnd)
+    padding_patterns = [
+        [1, 1, 1, 0, 1, 1, 0, 0], # 0xEC (236)
+        [0, 0, 0, 1, 0, 0, 0, 1]  # 0x11 (17)
+    ]
     
-    for inputbytePos in range(0, len(inputbinar)):
+    pattern_index = 0
+    while len(inputbinar) < max_bits:
+        current_pattern = padding_patterns[pattern_index % 2]
+        # Sicherstellen, dass wir nicht über max_bits hinausschießen
+        for bit in current_pattern:
+            if len(inputbinar) < max_bits:
+                inputbinar.append(bit)
+        pattern_index += 1
+
+    # --- PADDING LOGIK ENDE ---
+
+                    
+    #print(type(qr_code))
+    return inputbinar
+
+
+
+
+
+
+
+def dataANDKorrekturbits(link: str):
+    #link = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,]
+    qrFillorder = generate_qr_list() 
+    bits = fillNormal(link)
+    error_correction_bits = Error_correction_bits_erstellen(bits)
+
+    for i in range(len(error_correction_bits)):
+        bits.append(error_correction_bits[i])
+
+
+
+    for inputbytePos in range(0, len(bits)):
 
 
         for i, teil_liste in enumerate(qrFillorder):      # äußerer Index
             for j, pos in enumerate(teil_liste):          # innerer Index
                 if pos == inputbytePos + 13:
-                    qr_code[i][j] = inputbinar[inputbytePos]
+                    qr_code[i][j] = bits[inputbytePos]
                     print(inputbytePos)
-                    
-
-    return True
 
 
 
-"""
-print(fillNormal("Abc"))
-print(qr_code)
-erstelle_pixel_bild(qr_code)
+#print(fillNormal("Abc"))
+#print(qr_code)
+#erstelle_pixel_bild(qr_code)
 
-"""
 
 
 
