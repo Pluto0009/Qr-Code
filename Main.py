06d6d238627b75ebfc52bec01bbf028054dd1012
Pluto0009 <stein.Leopold@yahoo.com>
2026-03-09 01:05:35 +0100

    readme fertig

--- Main.py
+++ Main.py
@@ -5,12 +5,16 @@ from fill import dataANDKorrekturbits
 from gpu import erstelle_pixel_bild
 from Masking import apply_masking
 from FormatString import generate_format_string
+from linkshortner import shorten_url
+
 
 def GenerateQRCode(link: str):
-    # Reset qr_code IN PLACE (don't reassign the variable!)
     for i in range(qrcodevariable.rows):
         for j in range(qrcodevariable.cols):
             qrcodevariable.qr_code[i][j] = 0
+
+
+    link = shorten_url(link)
 
     qrcodevariable.link_length = len(link)
     qrcodevariable.fill_order_grid = Fillorder.generate_qr_list()
@@ -22,4 +26,4 @@ def GenerateQRCode(link: str):
     generate_format_string()
     erstelle_pixel_bild(qrcodevariable.qr_code)
 
-GenerateQRCode("https://www.google.com/")
+GenerateQRCode("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Qr-Code
 # Informatik Projekt mit Yannick und Leopold 
+
+
+benötigte Librarys
+
+python -m pip install pillow 
+python -m pip install pyshorteners
